f0292d4165185157633f59a3c096abadeca8f011
amongiants <jmgacnik@gmail.com>
2013-07-28 16:10:50 -0700

    Fixing search functionality

--- wp/wp-content/themes/xtra/search.php
+++ wp/wp-content/themes/xtra/search.php
@@ -7,44 +7,46 @@ $r_cb=1;
 $startNum_cb = $r_cb;
 $endNum_cb = 10;
 if($id_cb >=2) {
-  $s_cb=$id_cb-1;
-  $r_cb=($s_cb * 10) + 1;
-  $startNum_cb=$r_cb;
-  $endNum_cb=$startNum_cb + ($num_cb -1);
+	$s_cb=$id_cb-1;
+	$r_cb=($s_cb * 10) + 1;
+	$startNum_cb=$r_cb;
+	$endNum_cb=$startNum_cb + ($num_cb -1);
 } ?>
 
 
 <div id="search-wrap" class="padding-fix">
-	<h1 class="gothic meta-caps"><?php if (have_posts()) :
- 			echo "Showing <u>$startNum_cb-$endNum_cb</u> of ";
-		endif; ?><u><?=$wp_query->found_posts;?></u> results found for:&nbsp; <u><?php the_search_query() ?></u>
-	
+	<h1 class="gothic meta-caps">
+		<?php if (have_posts()) :
+			if ($wp_query->found_posts < $endNum_cb){
+				echo "Showing <u>$startNum_cb-$wp_query->found_posts</u> of ";
+			} else {
+				echo "Showing <u>$startNum_cb-$endNum_cb</u> of ";
+			}
+		endif; ?>
+			<u><?=$wp_query->found_posts;?></u> results found for:&nbsp; <u><?php the_search_query() ?></u>
 	</h1>
 
 	<ul>
 
-
-
-
-
-
-
-
-
-
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
-	if($post->post_type == 'post'){ 
+	if($post->post_type == 'event'){ 
 		$date = get_post_meta($post->ID, 'EVENT-date'); ?>
+
 		<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		<br><span class="gothic search-meta">—<?=$date[0]?></span><br></li>
-	<?php } else {
-		$ancestors = get_post_ancestors( $post );
-		$issue = get_the_title($ancestors[0]);
-		$volume = get_the_title($ancestors[1]);
-		$date = get_post_meta($ancestors[0], 'date'); ?>
+		<br><span class="gothic search-meta">—<?=$date[0]?></span><br>
+
+	<?php } else { 
+
+		$issue 	= wp_get_post_terms( $post->ID, 'issue_taxonomy', array("fields" => "names")); $issue = $issue[0]; 
+		$volume = wp_get_post_terms( $post->ID, 'volume_taxonomy', array("fields" => "names")); 
+		$volume = str_replace('v', 'Volume ', $volume[0]);
+		$season = explode(' ', $issue);
+		$number = $numbers[strtolower($season[0])];?>
+
 		<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		<br><span class="gothic search-meta">—<?=$date[0]?> <?=$volume?> <?=$issue?></span><br></li>
+		<br><span class="gothic search-meta">—<?=$issue?> <?=$volume?> <?=$number?></span><br>
+
 	<?php } ?>
 
 	<?php endwhile; else: ?>
